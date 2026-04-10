9d95dc8b6a1e080f4f500abad9a12db762cb37de
abhissshk <118242373+abhissshk@users.noreply.github.com>
2026-04-10 16:24:41 +0530

    Change default route to redirect all paths to login

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
 
     <Routes>
-      <Route path='/' element={<Navigate to="/login"/>}/>
+      <Route path='*' element={<Navigate to="/login"/>}/>
 <Route path='/login' element={<Login/>}/>
 <Route path='/signup' element={<Singup/>} />
 <Route path='/home' 
